51285a81a5a942b01b8500c311e6622b648690fc
sahale <phil.zabolotny@gmail.com>
2019-03-13 00:48:14 +0300

    Lesson 1 done

--- Lesson_1/2.py
+++ Lesson_1/2.py
@@ -1,3 +1,14 @@
 # 2. Выполнить логические побитовые операции "И", "ИЛИ" и др.
 # над числами 5 и 6. Выполнить
 # над числом 5 побитовый сдвиг вправо и влево на два знака.
+
+a = 5
+b = 6
+
+print('Bin a:', bin(a))
+print('Bin b:', bin(b))
+print('Bitwise AND:', bin(a & b))
+print('Bitwise OR:', bin(a | b))
+print('Bitwise EXC OR:', bin(a ^ b))
+print('Bitwise shift left:', bin(a << 2))
+print('Bitwise shift right:', bin(a >> 2))
--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,3 +1,23 @@
 # 9.Вводятся три разных числа. Найти, какое из них
 # является средним (больше одного, но меньше другого).
 
+a, b, c = input('Введите числа через пробел: ').split()
+a, b ,c = float(a), float(b), float(c)
+
+if a > b and a > c:
+    if b > c:
+        print(b)
+    else:
+        print(c)
+elif b > a and b > c:
+    if a > c:
+        print(a)
+    else:
+        print(c)
+elif c > a and c > b:
+    if a > b:
+        print(a)
+    else:
+        print(b)
+else:
+    print('Среднего нет!')
