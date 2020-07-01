8a9dc27d50c1c3a19ea603dc75cc5f1229325c69
philz <NewH@$kerBl0c768B!Tb>
2020-07-01 21:24:50 +0300

    4: 1-2 done

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,34 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+import cProfile
+
+
+def cycle():
+    n = 32
+    # '+ 1' чтобы таблица печаталась полностью
+    for i in range(((127 - 32) // 10) + 1):
+        y = 0
+        while y < 10 and n <= 127:
+            print('num:', '%3d' % n, chr(n), end='|')
+            n += 1
+            y += 1
+        print()
+
+
+# Мы будем тут только передавать номер числа
+def recursion(num):
+    if num <= 127:
+        if num % 10 == 2 and num != 32:
+            print()
+        print('num:', '%3d' % num, chr(num), end='|')
+        num += 1
+        recursion(num)
+#
+# if __name__ == '__main__':
+#     # cycle()
+#     recursion()
+
+cProfile.run('cycle()')
+num = 32
+cProfile.run('recursion(num)')
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,55 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+import timeit
+import cProfile
+
+
+def resh(num):
+    n = 10000
+    sieve = [x for x in range(n)]
+    sieve[1] = 0    # без этой строки итоговый список будет содержать единицу
+    for i in sieve:
+        if i > 1:
+            for j in range(i + i, len(sieve), i):
+                sieve[j] = 0
+    return [p for p in sieve if p != 0][num - 1]
+
+
+def notresh(el):
+    n = 10000
+    l = [i for i in range(n)]
+    fin = []
+    for i in l:
+        j = 2
+        x = 0
+        if i == 0 or i == 1:
+            continue    # без этой строки итоговый список будет содержать ноль и единицу
+        while j < i:
+            if l[i] % l[j] == 0:
+                x = 1
+            j += 1
+        if x == 0:
+            fin.append(l[i])
+        i += 1
+        if len(fin) == el:
+            return fin[el - 1]
+
+
+def main():
+    try:
+        el = int(input('Введите i-ый элемент: '))
+    except ValueError:
+        print('Ты пидор')
+        main()
+    print(resh(el))
+    print(notresh(el))
+    print(timeit.timeit('resh(161)', setup='from __main__ import resh', number=1000))
+    print(timeit.timeit('notresh(161)', setup='from __main__ import notresh', number=1000))
+    cProfile.run('resh(161)')
+    cProfile.run('notresh(161)')
+
+
+if __name__ == '__main__':
+    main()
+
